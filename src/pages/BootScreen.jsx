9d97defd485d25abe12d821e155eebc8bb6b595c
zaki-s <zakijames024@gmail.com>
2025-08-29 18:07:27 +0300

    setting up

---
+++ src/pages/BootScreen.jsx
@@ -0,0 +1,23 @@
+
+
+import { useEffecr, useState } from "react";
+
+const fakeLogs = [
+  "Initializing system...",
+ "[ OK ] Starting system logger (syslogd)...",
+  "[ OK ] Initializing hardware monitors...",
+  "[ OK ] Loading kernel modules: netfilter, overlayfs, ext4...",
+  "[ OK ] Mounting /dev/sda1 to /root...",
+  "[ OK ] Checking filesystem integrity... clean.",
+  "[ OK ] Bringing up network interface eth0...",
+  "[ OK ] DHCP lease acquired: 192.168.0.42",
+  "[ OK ] Starting SSH daemon on port 22...",
+  "[ OK ] Starting HTTP server on port 8080...",
+  "[ OK ] Loading environment variables...",
+  "[ OK ] Compiling React components...",
+  "[ OK ] Applying CSS stylesheets...",
+  "[ OK ] Connecting to GitHub repository...",
+  "[ OK ] Fetching project data...",
+  "[ OK ] Loading Zaki’s Portfolio Engine v1.0.0...",
+  "[ OK ] Initializing cinematic intro sequence...",
+  "[ OK ] System ready. Launching main UI..."
