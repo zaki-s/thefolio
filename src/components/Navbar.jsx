3e5bb9375e6c8a33cd96523b6a2714749c2d70f3
zaki-s <zakijames024@gmail.com>
2025-09-02 20:52:59 +0300

    added fonts that match the whole dev aesthetic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,15 @@ const Navbar = () => {
     <div className="navbar">
         <h1 className="title">the1</h1>
         <div className="links">
-            <a href="#aboutme">I.About</a>
-            <a href="#projects">II.Education/Experience</a>
-            <a href="#contact">III.Projects</a>
-            <a href="#contact">IV.Contact</a>    
+            <a href="#aboutme"><span className="roman">01</span> About</a>
+            <a href="#projects"><span className="roman">02</span> Education/Experience</a>
+            <a href="#contact"><span className="roman">03</span> Projects</a>
+            <a href="#contact"><span className="roman">04</span> Contact</a>
             <a href="/resume.pdf" download="Zaki's Resume.pdf" className="navbtn">
-           Resume
-        </a>
+                <span className="navbtntxt">Resume</span>
+            </a>
         </div>
+
     </div>
   )
 }
