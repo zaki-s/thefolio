f808301c6b7f8cc381f398415c25cc46f23fd8b5
zaki-s <zakijames024@gmail.com>
2025-09-02 21:20:18 +0300

    added more color over the links upon hovering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
     <div className="navbar">
         <h1 className="title">the1</h1>
         <div className="links">
-            <a href="#aboutme"><span className="roman">01</span> About</a>
-            <a href="#projects"><span className="roman">02</span> Education/Experience</a>
-            <a href="#contact"><span className="roman">03</span> Projects</a>
-            <a href="#contact"><span className="roman">04</span> Contact</a>
+            <a href="#aboutme" className="alinks"><span className="roman">01.</span> About</a>
+            <a href="#projects" className="alinks"><span className="roman">02.</span> Education/Experience</a>
+            <a href="#contact" className="alinks"><span className="roman">03.</span> Projects</a>
+            <a href="#contact" className="alinks"><span className="roman">04.</span> Contact</a>
             <a href="/resume.pdf" download="Zaki's Resume.pdf" className="navbtn">
                 <span className="navbtntxt">Resume</span>
             </a>
