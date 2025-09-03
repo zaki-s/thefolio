e5a66dcc62b58ae3a4a3b28ecc82b7ce4601e99f
zaki-s <zakijames024@gmail.com>
2025-09-03 21:10:21 +0300

    added the details about me to highlight aspects about me and also tech i use

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,24 +2,67 @@
 
 import React from 'react'
 import Tilt from 'react-parallax-tilt'
+import { FaReact, FaNodeJs, FaPython, FaJava, FaHtml5, FaCss3Alt, FaGithub } from "react-icons/fa";
+import { SiJavascript, SiTailwindcss, SiPostgresql, SiFlask } from "react-icons/si";
+import TiltedCard from './TiltedCard'
+import the1 from '../assets/images/the1.jpg'
 import "./About.css"
 
 const About = () => {
   return (
-    <div>
+    <div className="abt">
+    <div className="abtcont">
         <div className="abttext">
             <h3>Hi, my name is</h3><br></br>
-            <h1>Zaki James</h1><br></br>
+            <h1>Zaki James.</h1><br></br>
             <h2>I love creating stuff!!</h2>
             <p>I am a full stack software engineer who loves building</p>
             <p>and creating digital experiences. I also love the whole essence</p>
-            <p>of creating, in ay aspect! And i love comics and cars!!</p>
+            <p>of creating, in any aspect! And i love comics and cars!!</p>
         </div>
-        <Tilt glareEnable={true} glareMaxOpacity={0.45} scale={1.05} transitionSpeed={2500}>
-        <div className="abtfoto">
-          <img src="../assets/images/the1.png" alt="Zaki" />
-        </div>
-      </Tilt>
+        <TiltedCard
+            imageSrc={the1}
+            altText="zaki james"
+            captionText="Zaki James - Software Engineer"
+            containerHeight="300px"
+            containerWidth="300px"
+            imageHeight="300px"
+            imageWidth="300px"
+            rotateAmplitude={12}
+            scaleOnHover={1.2}
+            showMobileWarning={false}
+            showTooltip={true}
+            displayOverlayContent={true}
+            overlayContent={
+            <p className="tilted-card-demo-text">
+            the1
+            </p>
+            }
+        />
+    </div>
+    <div className="abtyap">
+        <div className="abtech">
+        <h3>Technologies I use:</h3>
+          <div className="tech-icons">
+            <FaHtml5 className="icon html" />
+            <FaCss3Alt className="icon css" />
+            <SiJavascript className="icon js" />
+            <FaReact className="icon react" />
+            <SiTailwindcss className="icon tailwind" />
+            <FaNodeJs className="icon node" />
+            <FaPython className="icon python" />
+            <SiFlask className="icon flask" />
+            <SiPostgresql className="icon postgres" />
+            <FaGithub className="icon github" />
+          </div>
+      </div>
+      
+      <div className="yap">
+        <p>My journey into tech started with curiosity about how games are built. I was a Mech.Eng major but it didn't quite spark the fire in me, see i prefer tech, influenced by comics games and movies. I left Mech and started learning Software.dev on my own using free resources.</p>
+        <p>After a year of learning by myself i decided to better my skills and also network as well, I got into Moringa School to do that! and that was really insightfull. I learned lots of things there.</p>
+        <p>Tech is not just my career path, it’s how I explore, experiment, and build the future I want. And i just love creating! Films, Stories... I just love it</p>
+      </div>
+    </div>
     </div>
   )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,13 @@
  
 import React from 'react'
 import Navbar from '../components/Navbar'
+import About from '../components/About'
 
 const Main = () => {
   return (
-    <div>
+    <div className="maincont">
         <Navbar />
+        <About />
 
     </div>
   )
