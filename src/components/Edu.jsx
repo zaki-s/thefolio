dcbb2e57304a58fd7e904e7a3d6d6c4faf7028af
zaki-s <zakijames024@gmail.com>
2025-09-08 14:29:20 +0300

    linked workplaces/schools to websites

--- src/components/Edu.jsx
+++ src/components/Edu.jsx
@@ -22,23 +22,23 @@ const Edu = () => {
             <ul>
               <li>
                 <h4>2025 Feb - 2025 Aug</h4>
-                <p>Moringa school</p>
-                <p>I enrolled into Moringa to upskill and also connect with like minded people in the same field and I learned so much in my time there, collaboration, team leading and overally the whole deeper essence of developing.</p>
+                <p className="edusectitle"><a className="area" href="https://moringaschool.com/" target="/blank">Moringa school</a></p>
+                <p className="edusectyap">I enrolled into Moringa to upskill and also connect with like minded people in the same field and I learned so much in my time there, collaboration, team leading and overally the whole deeper essence of developing.</p>
               </li>
               <li>
                 <h4>2023 Nov - 2025 Feb</h4>
-                <p>Self Learning (Software Engineering)</p>
-                <p>I took a step to start lerning something i like and love, using free resources like free codecamp, Scrimba and the most used of all, YOUTUBE. It was a bit hectic but i learned alot during this time!</p>
+                <p className="edusectitle">Self Learning (Software Engineering)</p>
+                <p className="edusectyap">I took a step to start lerning something i like and love, using free resources like free codecamp, Scrimba and the most used of all, YOUTUBE. It was a bit hectic but i learned alot during this time!</p>
               </li>
               <li>
                 <h4>2020 Jan - 2021 October</h4>
-                <p>Thika Technical Training Institute</p>
-                <p>I enrolled to get a Certificate and Diploma in Mech.Eng but it wasnt really my thing, it didnt quite make me feel happy and so i left it behind to pursue something that gives me joy!</p>
+                <p className="edusectitle">Thika Technical Training Institute</p>
+                <p className="edusectyap">I enrolled to get a Certificate and Diploma in Mech.Eng but it wasnt really my thing, it didnt quite make me feel happy and so i left it behind to pursue something that gives me joy!</p>
               </li>
               <li>
                 <h4>2016 - 2019</h4>
-                <p>Kenyatta Highschool</p>
-                <p>KCSE Certificate</p>
+                <p className="edusectitle">Kenyatta Highschool</p>
+                <p className="edusectyap">KCSE Certificate</p>
               </li>
             </ul>
           </div>
@@ -48,15 +48,15 @@ const Edu = () => {
             <ul>
               <li>
                 <h4>2023 June - 2024 March</h4>
-                <p>Commercial Agribusiness</p>
-                <p>Lead Superviser (Personall Business)</p>
-                <p>I invested into commercial farming in Taveta, producing Tomatoes as the main produce and selling to vendors in Mombasa, Taveta and sometimes Tanzania as well. Challenges faced were breaking into market due to my age, but in the long run it really taught me how to run a business lead a whole team and have a good rapport between you (a leader) and your workers.</p>
+                <p className="edusectitle">Commercial Agribusiness</p>
+                <p className="role">Lead Supervisor (Personall Business)</p>
+                <p className="edusectyap">I invested into commercial farming in Taveta, producing Tomatoes as the main produce and selling to vendors in Mombasa, Taveta and sometimes Tanzania as well. Challenges faced were breaking into market due to my age, but in the long run it really taught me how to run a business lead a whole team and have a good rapport between you (a leader) and your workers.</p>
               </li>
               <li>
                 <h4>2021 Feb - 2021 April</h4>
-                <p>Sichey Automotive</p>
-                <p>Technical Personell</p>
-                <p>DUring this short period that was an attachment, i served in the Technical team in the company Servicing Cars and fixing Technical issues. The stay there instilled skills on working with otthers and also working under pressure as well!</p>
+                <p className="edusectitle"><a className="area" href="https://www.sicheyautomotive.com/" target="/blank">Sichey Automotive</a></p>
+                <p className="role">Technical Personell</p>
+                <p className="edusectyap">DUring this short period that was an attachment, i served in the Technical team in the company Servicing Cars and fixing Technical issues. The stay there instilled skills on working with otthers and also working under pressure as well!</p>
               </li>
             </ul>
           </div>
