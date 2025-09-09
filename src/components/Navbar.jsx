af479737657941a8507d3a3d9a04c01e66f2b456
zaki-s <zakijames024@gmail.com>
2025-09-09 13:03:04 +0300

    fixed mobile links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
         <a href="/m#edu" onClick={() => setMenuOpen(false)}>Education/Experience</a>
         <a href="/m#projects" onClick={() => setMenuOpen(false)}>Projects</a>
         <a href="/m#contact" onClick={() => setMenuOpen(false)}>Contact</a>
-        <a href="/Zaki's Resume.pdf" download="Zaki's Resume.pdf">Resume</a>
+        <a href="/folio.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
       </motion.div>
     </motion.div>
   );
